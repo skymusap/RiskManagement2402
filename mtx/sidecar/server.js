c736ae46e7a67b8e7c345f3255a0bdb29f487c2b
Sky Mu <sky.mu@sap.com>
2024-02-28 03:47:10 +0000

    Enabling Multitenancy for the Previously Created Application Step 3 update server.js

--- mtx/sidecar/server.js
+++ mtx/sidecar/server.js
@@ -1,6 +1,24 @@
 const cds = require('@sap/cds');
 const { MTX } = require('@sap/low-code-event-handler/mtx');
 const LOG = cds.log('lcap-mtx')
+const xsenv = require('@sap/xsenv');
+cds.on('served', async () => {
+  const { 'cds.xt.SaasProvisioningService':provisioning } = cds.services
+    await provisioning.prepend(() => {
+      provisioning.on('dependencies', async (req, next) => {
+        await next()
+        const services = xsenv.getServices({
+          dest: { label: 'destination' }
+        })
+        let dependencies = [
+          {
+            xsappname: services.dest.xsappname
+          }
+        ]
+        return dependencies
+      })
+    })
+})
 try {         
     MTX.initRoute();  
 } catch (error) {
