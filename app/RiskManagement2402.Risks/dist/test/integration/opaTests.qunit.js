46bf7d356b76130c028f8bd3cbd35b223b199449
Sky Mu <sky.mu@sap.com>
2024-02-28 02:43:19 +0000

    Deploy the Application to the Cloud Foundry Environment

---
+++ app/RiskManagement2402.Risks/dist/test/integration/opaTests.qunit.js
@@ -0,0 +1,2 @@
+sap.ui.require(["sap/fe/test/JourneyRunner","RiskManagement2402/Risks/test/integration/FirstJourney","RiskManagement2402/Risks/test/integration/pages/RisksList","RiskManagement2402/Risks/test/integration/pages/RisksObjectPage"],function(e,s,i,t){"use strict";var e=new e({launchUrl:sap.ui.require.toUrl("RiskManagement2402/Risks")+"/index.html"});e.run({pages:{onTheRisksList:i,onTheRisksObjectPage:t}},s.run)});
+//# sourceMappingURL=opaTests.qunit.js.map
